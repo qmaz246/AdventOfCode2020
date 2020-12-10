c8608021c9869d7fa16fe848d21ff1ab96939aef
Quinn Mazzilli <qmazzilli@laughtale.mamd.g3ti.local>
2020-12-10 14:50:47 -0500

    Started Day 10

---
+++ Day10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main(void){
+	FILE * fp;
+	char line[6];
+	
+	fp = fopen("Day10_sample.txt", "r");
+	while(fgets(line, 6, fp)){
+		
+	}
+
+}
